a8fd012b30254de30e0af30ac5e20de5dd182f67
dev-aditya-lab <ad1123itya@gmail.com>
2026-05-22 15:42:19 +0530

    feat: add logger middleware for request logging

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import corsConfig from './config/cors.config.js';
+import logger from './core/middlewares/logger.middleware.js';
 const app = express();
 
 app.use(corsConfig);
+app.use(logger);
 
 
 export default app;
