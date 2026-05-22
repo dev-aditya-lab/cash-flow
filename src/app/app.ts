f48198c4c1b3af82c7039fe762f5228f7d523fdc
dev-aditya-lab <ad1123itya@gmail.com>
2026-05-22 16:02:11 +0530

    refactor: update development scripts and add response utility
    - Changed the dev script to use 'tsx' for better TypeScript support during development.
    - Added 'ts-node' as a dependency for TypeScript execution.
    - Included 'nodemon' and 'tsx' in devDependencies for improved development experience.
    - Enhanced the app by adding a welcome route that responds with a success message and application details.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,18 @@
-import express from 'express';
-import corsConfig from './config/cors.config.js';
-import logger from './core/middlewares/logger.middleware.js';
+import express from "express";
+import type { Request, Response } from "express";
+import corsConfig from "./config/cors.config.js";
+import logger from "./core/middlewares/logger.middleware.js";
+import sendRes from "./core/response/sucess.response.js";
 const app = express();
 
 app.use(corsConfig);
 app.use(logger);
 
+app.get("/", (_req: Request, res: Response): void => {
+	sendRes(res, 200, "Welcome to the Express TypeScript API", {
+		version: "1.0.0",
+		app: "CashFlow"
+	});
+});
 
 export default app;
