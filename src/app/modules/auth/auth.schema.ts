1ed9473a7a211c7b3df403ff59585bac9576d457
dev-aditya-lab <ad1123itya@gmail.com>
2026-05-22 17:46:50 +0530

    feat: implement password hashing and comparison methods in auth schema

--- src/app/modules/auth/auth.schema.ts
+++ src/app/modules/auth/auth.schema.ts
@@ -1,4 +1,6 @@
 import mongoose, { Schema, Document } from "mongoose";
+import bcrypt from "bcrypt";
+import config from "../../config/env.config.js";
 
 export interface IAuth extends Document {
 	email: string;
@@ -36,6 +38,32 @@ const authSchema = new Schema<IAuth>(
 	},
 	{ timestamps: true },
 );
+
+authSchema.pre<IAuth>("save", async function () {
+	if (!this.isModified("password")) return;
+	try {
+		const salt = await bcrypt.genSalt(10);
+		this.password = await bcrypt.hash(this.password, salt);
+	} catch (error) {
+		if (config.nodeEnv === "development") {
+			console.error("Error hashing password:", error);
+		}
+		throw new Error("Password hashing failed");
+	}
+});
+
+authSchema.methods.comparePassword = async function (
+	candidatePassword: string,
+): Promise<boolean> {
+	try {
+		return await bcrypt.compare(candidatePassword, this.password);
+	} catch (error) {
+		if (config.nodeEnv === "development") {
+			console.error("Error comparing password:", error);
+		}
+		throw new Error("Password comparison failed");
+	}
+};
 
 const AuthModel = mongoose.model<IAuth>("Auth", authSchema);
 export default AuthModel;
