fa49d0532f75fc4203b606576df47e889d9382fb
dev-aditya-lab <ad1123itya@gmail.com>
2026-05-26 19:02:24 +0530

    Add TWA manifest for CashFlow app with initial configuration

---
+++ mobile/app/src/main/java/cashflow/devaditya/dev/DelegationService.java
@@ -0,0 +1,14 @@
+package cashflow.devaditya.dev;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
